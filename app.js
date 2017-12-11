0a22718b1bc7e9a890f1f8fc1a6489de2506f7e4
Dewi Nurdin <dewinurdin@gmail.com>
2017-12-11 17:59:00 -0600

    created rendered button

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
-// Variable API to hold URL
-	var url = "https://api.giphy.com/v1/gifs/search?api_key=iW1s83WhVS4y6wqeHCOFOKdAwNKH6TUt&q=";
-	var apiKey = "&limit=25&offset=0&rating=G&lang=en";
-	var queryURL = url + santa + apiKey;
-	var santa = "santa%20animated";
-	var rudolph = "rudolph%20the%20red%20nose%20raindeer";
+$(document).ready(function() {
+	console.log("ready");
+	// this is array of topic to renderButtons
 
+ var holiday = ["santa", "rudolph", "elves", "northpole"];
+
+// write a function to display gif from API
+function showGif () {
+	$(document).on("click", ".christmas", function() {
+		$("#gif-images").empty();
+		var topic = $(this).attr("data-giphy");
+		var giphyUrl ="https://api.giphy.com/v1/gifs/search?api_key=iW1s83WhVS4y6wqeHCOFOKdAwNKH6TUt&q=" + topic + "&limit=25&offset=0&rating=G&lang=en";
+		console.log(giphyUrl);
 		$.ajax({
-		url: queryURL,
+		url: giphyUrl,
 		Method: "GET"
-		}).done(function(response) {
+		}) .done(function(response) {
 			console.log(response);
 
-		var giphyUrl = response.data.data.images.original.url;
-		giphyUrl.attr("src", giphyUrl);
-		giphyUrl.attr("alt", "giphy images");
-		});
+		var results = response.data;
 
-// Create Click function on buttons to append gif images to html
+				for (var i=0; i<results.length; i++) {
+				//show images
+				var showImage = $("<img>");
+				showImage.attr("src", results[i].data.images.original.url);
+								
+			
+			}		
+		})
+	})
+}; 
+
+//write a function to dynamically create a button
+ function renderButton() {
+ 	for (var i=0; i<holiday.length; i++) {
+ 		var buttons = $("<button>");
+ 		 	buttons.addClass("christmas");
+ 		 	buttons.addClass("btn");
+ 		 	buttons.attr("data-giphy", holiday[i]);
+ 		 	buttons.text(holiday[i]);
+ 		 	$("#buttons-holder").append(buttons);
+ 	}
+ }
+	renderButton();
+
+	showGif();
+
+// Create Renderbuttons
+
+// Create on Click function on buttons to append gif images to html when button is clicked
+//BUTTON TRIGGERED ajax
 
 	$(document).on("click", "#button1", function(){
 		$("#gif-images").append(giphyUrl);
@@ -24,7 +56,7 @@
 
 // setting a pause state and animate
 		
-	
+	})
 
 
 
