e5a5f9445c4554571c1d068d2ed12c186d51957c
Dewi Nurdin <dewinurdin@gmail.com>
2017-12-16 01:06:22 -0600

    added pause and animated state function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
- var topic = ["Santa Claus", "Mrs. Claus", "Rudolph The Red Nose Raindeer", "Little Drummer Boy", "The Grinch", "Santa's Elves", "Santa's Northpole", "Frosty the Snowman", "Christmas Trees", "The birth of Jesus", "Christmas Angels", "Scrooge"];
+ var topic = ["Santa Claus", "Jingle Bells", "Rudolph The Red Nose Raindeer", "Little Drummer Boy", "The Grinch", "Twelve Days of Christmas", "Santa's Northpole", "Frosty the Snowman", "The Birth of Jesus", "Scrooge"];
  var api = "https://api.giphy.com/v1/gifs/search?api_key=iW1s83WhVS4y6wqeHCOFOKdAwNKH6TUt&q=";
  var apiKey = "&limit=25&offset=0&rating=G&lang=en";
 
@@ -8,13 +8,16 @@ $(document).ready(function() {
  function renderButton() {
  	for (var i=0; i<topic.length; i++) {
  		var buttons = $("<button>");
- 		 	buttons.addClass("gif");
- 		 	buttons.addClass("btn");
- 		 	buttons.attr("data-animate", topic[i]);
+ 			buttons.attr({
+ 				"class": "christmas btn",
+ 				"data-still": topic[i],
+ 			})
  		 	buttons.text(topic[i]);
- 		 	$("#buttons-holder").append(buttons);
+
+ 		 	$("#buttons-holder").append(buttons); 		 	
  	}
  };
+
 renderButton();
 
 $("#add-topic").on("click", function(event) {
@@ -23,79 +26,67 @@ $("#add-topic").on("click", function(event) {
 		var userInput = $("#topic-form").val();
 			topic.push(userInput);
 		var button = $("<button>");
-			button.addClass("btn");
-			button.addClass("gif");
-			button.attr("data-animate", userInput);
+			button.attr({
+				"class": "christmas btn",
+				"data-still": userInput,				
+			})
 			button.text(userInput);
+
 			$("#buttons-holder").append(button);
 
-	});
+});
+
+//clear out form after user inputted value and appended dynamic button
+// NOT WORKING!!
+//$("#topic-form").empty(); 
 
 // write a function to display gif from API
-// Create on Click function on buttons to append gif images to html when button is clicked
-//BUTTON TRIGGERED ajax
-function showGif() {
-	$(document).on("click", ".gif", function() {
+// Create on Click function on buttons to append gif images when button is clicked
+
+function showGif() {	
+	$(".christmas").on("click", function() {
 		$("#gif-images").empty();
-		var topic = $(this).attr("data-animate");
-		var giphyUrl = api + topic + apiKey;
-		//console.log(giphyUrl);
+		var state = $(this).attr("data-still");
+		
+		//console.log("click button")
+		var giphyUrl = api + state + apiKey;
+		console.log(giphyUrl);
+
 		$.ajax({
 		url: giphyUrl,
 		Method: "GET"
 		}) .done(function(response) {
-			console.log(response.data);
-
+			console.log(response);
+		
 		var results = response.data;
 			for (var i=0; i<results.length; i++) {
 		//show images
-		var showImage = $("<img class = gif>");
-			showImage.attr("src", results[i].images.fixed_height.url);
-			showImage.attr("data-state: 'animate'");
-		
+		var showImage = $("<img>");
+			showImage.attr({
+					"class" : "gif",
+					"src" : results[i].images.fixed_height_still.url, 
+					"data-animate": results[i].images.fixed_height.url,
+					"data-still": results[i].images.fixed_height_still.url,
+					"data-state": "still"					
+			});
+
 		$("#gif-images").append(showImage);
 
-		var dataStill = $("<img, class = gif>");
-			dataStill.attr("src", results[i].images.fixed_height_still.url);
-			dataStill.attr("data-state: 'still'");
+		// setting a pause state and animate
+		$(".gif").on("click", function(){
+			
 
-		
-			}		
+			if (state === "still") {
+	        $(this).attr(showImage.src, $(this).attr("data-animate"));
+	        $(this).attr("data-state", "animate");
+	      	} else {
+	        $(this).attr("src", $(this).attr("data-still"));
+	        $(this).attr("data-state", "still");
+	      }
 		})
-	})
-}; 
-
+   	}		
+})
+});
+}
 showGif();
-
-// setting a pause state and animate
-
-// $(document).on("click", ".gif", function(){
-// 	var state = $(this).attr(results.images.fixed_height_still.url);
-// 	if (state === "animate") {
-// 		$(this).attr()
-// 	}
-		
-// })
-	
-//click function to trigger AJAX call
-
-
-
-
-}) //DOC.ready
-
-
-
-
-
-	
-	
-
-
-
-
-
-	
-
-
-
+});
